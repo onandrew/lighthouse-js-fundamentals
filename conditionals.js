e6f163011003381545cf60dbc0385bdcebdb4917
Andrew On <a_on93@hotmail.com>
2023-07-05 13:59:54 -0400

    added extra conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,13 @@
-const temperature = -112;
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+const temperature = 12;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
