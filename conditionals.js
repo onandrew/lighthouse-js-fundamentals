1eb20ce93793244508c83858573192342bdf9bd5
Andrew On <a_on93@hotmail.com>
2023-07-12 10:33:26 -0400

    adding conditionals file

--- conditionals.js
+++ conditionals.js
@@ -1,20 +1,15 @@
-const isCitizen = true;
-const age = 26;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
+var age;
+var school;
+const whichSchool  = function (age) {
+  if (age < 13) {
+    school = "Elementary School";
+  } 
+  else if (age >= 13 && age <= 18) {
+    school = "Secondary School";
+  }
+  else {
+    school = "Lighthouse Labs";
+  }
+  console.log("I am " + age + ". Which school should I go to?");
+  console.log(school);
 }
-if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea…");
-}
-const temperature = 12;
-
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
-
-console.log("Now you're ready to go outside!");
